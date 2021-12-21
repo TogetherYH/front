16ba6e0d72083b9efe789d976eab8fee1c4386fd
hefang <36717424@qq.com>
2021-12-21 13:56:59 +0800

    home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,10 @@
-import styles from './index.less';
-
-import { Space, Button } from 'antd';
-
 import { useModel } from 'umi';
 
 export default function IndexPage() {
   const { initialState } = useModel('@@initialState');
-  const { user, login, logout } = useModel('login');
   return (
     <div>
-      <h1 className={styles.title}>
-        {' '}
-        {initialState?.currentUser?.username}, Hello~
-      </h1>
-      {/* <h1 className={styles.title}> { user?.username }, Hello ~</h1> */}
-      <Space>
-        <Button
-          type="primary"
-          onClick={() => {
-            login('admin', '123');
-          }}
-        >
-          登录
-        </Button>
-        <Button
-          type="primary"
-          onClick={() => {
-            logout();
-          }}
-        >
-          登出
-        </Button>
-      </Space>
+      <h1>{initialState?.currentUser?.username}, Hello~</h1>
     </div>
   );
 }
