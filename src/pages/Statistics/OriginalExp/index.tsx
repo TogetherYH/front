a0cab7b06e54b25ab6ebf3be62d4cb9f7ac9f183
hefang <36717424@qq.com>
2022-07-04 14:33:34 +0800

    修改原始数据导出表格渲染方式

--- src/pages/Statistics/OriginalExp/index.tsx
+++ src/pages/Statistics/OriginalExp/index.tsx
@@ -1,9 +1,8 @@
-import { FC, useState } from 'react';
+import { FC, useState, useRef, useEffect } from 'react';
 import {
   TreeSelect,
   Space,
   Form,
-  Input,
   Button,
   Card,
   Select,
@@ -24,6 +23,9 @@ import {
   deptTreeState,
   originalState,
 } from 'umi';
+import { VariableSizeGrid as Grid } from 'react-window';
+import classNames from 'classnames';
+import ResizeObserver from 'rc-resize-observer';
 import moment from 'moment';
 import { exp } from './service';
 
@@ -40,7 +42,6 @@ interface OriginalExpProps {
 const OriginalExp: FC<OriginalExpProps> = (prop) => {
   const {
     scaleTree,
-    publishs,
     deptTree,
     publishSelect,
     original,
@@ -53,6 +54,128 @@ const OriginalExp: FC<OriginalExpProps> = (prop) => {
 
   const searchHandler = () => {
     searchForm.submit();
+  };
+
+  const VirtualTable = (props: Parameters<typeof Table>[0]) => {
+    const { columns, scroll } = props;
+    const [tableWidth, setTableWidth] = useState(0);
+
+    console.log('tableWidth', tableWidth);
+
+    const widthColumnCount = columns!.filter(({ width }) => !width).length;
+    const mergedColumns = columns!.map((column) => {
+      if (column.width) {
+        return column;
+      }
+
+      return {
+        ...column,
+        width: Math.floor(tableWidth / widthColumnCount),
+      };
+    });
+
+    const gridRef = useRef<any>();
+    const [connectObject] = useState<any>(() => {
+      const obj = {};
+      Object.defineProperty(obj, 'scrollLeft', {
+        get: () => null,
+        set: (scrollLeft: number) => {
+          if (gridRef.current) {
+            gridRef.current.scrollTo({ scrollLeft });
+          }
+        },
+      });
+
+      return obj;
+    });
+
+    const resetVirtualGrid = () => {
+      if (gridRef.current) {
+        gridRef.current.resetAfterIndices({
+          columnIndex: 0,
+          shouldForceUpdate: true,
+        });
+      }
+    };
+
+    useEffect(() => resetVirtualGrid, [tableWidth]);
+
+    const renderVirtualList = (
+      rawData: object[],
+      { scrollbarSize, ref, onScroll }: any,
+    ) => {
+      ref.current = connectObject;
+      const totalHeight = rawData.length * 54;
+
+      return (
+        <Grid
+          ref={gridRef}
+          className="virtual-grid"
+          columnCount={mergedColumns.length}
+          columnWidth={(index: number) => {
+            const { width } = mergedColumns[index];
+            return totalHeight > scroll!.y! &&
+              index === mergedColumns.length - 1
+              ? (width as number) - scrollbarSize - 1
+              : (width as number);
+          }}
+          height={scroll!.y as number}
+          rowCount={rawData.length}
+          rowHeight={() => 54}
+          width={tableWidth}
+          onScroll={({ scrollLeft }: { scrollLeft: number }) => {
+            onScroll({ scrollLeft });
+          }}
+        >
+          {({
+            columnIndex,
+            rowIndex,
+            style,
+          }: {
+            columnIndex: number;
+            rowIndex: number;
+            style: React.CSSProperties;
+          }) => (
+            <div
+              className={classNames('virtual-table-cell', {
+                'virtual-table-cell-last':
+                  columnIndex === mergedColumns.length - 1,
+              })}
+              style={{
+                ...style,
+                padding: '16px',
+                borderBottom: '1px solid #e8e8e8',
+              }}
+            >
+              {
+                (rawData[rowIndex] as any)[
+                  (mergedColumns as any)[columnIndex].dataIndex
+                ]
+              }
+            </div>
+          )}
+        </Grid>
+      );
+    };
+
+    return (
+      <ResizeObserver
+        onResize={({ width }) => {
+          console.log('ww', width);
+          setTableWidth(width);
+        }}
+      >
+        <Table
+          {...props}
+          className="virtual-table"
+          columns={mergedColumns}
+          pagination={false}
+          components={{
+            body: renderVirtualList,
+          }}
+        />
+      </ResizeObserver>
+    );
   };
 
   const onFinish = (values: any) => {
@@ -169,7 +292,7 @@ const OriginalExp: FC<OriginalExpProps> = (prop) => {
         </Card>
         <Card>
           <Spin spinning={exporting}>
-            <Table
+            {/* <Table
               dataSource={original.list}
               columns={original.columns}
               size="small"
@@ -182,7 +305,20 @@ const OriginalExp: FC<OriginalExpProps> = (prop) => {
               }}
               rowKey="resultId"
               pagination={false}
-            ></Table>
+            ></Table> */}
+            <VirtualTable
+              columns={original.columns ? original.columns : [{ width: 80 }]}
+              size="small"
+              loading={listLoading}
+              dataSource={original.list ? original.list : []}
+              rowKey="resultId"
+              scroll={{
+                x: original.columns?.length
+                  ? original.columns?.length * 80
+                  : 2000,
+                y: 600,
+              }}
+            />
           </Spin>
         </Card>
       </Space>
