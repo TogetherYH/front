acb56f7b0063f2f6af338ac1e367635adb103c3a
hefang <36717424@qq.com>
2022-07-13 16:46:34 +0800

    按月统计

--- src/pages/DV/components/YearColumn.tsx
+++ src/pages/DV/components/YearColumn.tsx
@@ -2,69 +2,72 @@ import { FC, useState, useEffect } from 'react';
 import { Card } from 'antd';
 // import { Column } from '@ant-design/charts';
 import { Column } from '@ant-design/plots';
+import { connect, Dispatch, resultByMonthState } from 'umi';
 import styles from '../index.less';
 
-interface YearColumnProps {}
+interface YearColumnProps {
+  resultByMonth: resultByMonthState;
+}
 
-const YearColumn: FC<YearColumnProps> = ({}) => {
-  const data = [
-    {
-      date: '2021-8',
-      scale: 'SCL-90',
-      times: 203,
-    },
-    {
-      date: '2021-9',
-      scale: 'SCL-90',
-      times: 803,
-    },
-    {
-      date: '2021-10',
-      scale: 'SCL-90',
-      times: 453,
-    },
-    {
-      date: '2021-11',
-      scale: 'SCL-90',
-      times: 343,
-    },
-    {
-      date: '2021-12',
-      scale: 'SCL-90',
-      times: 223,
-    },
-    {
-      date: '2022-1',
-      scale: 'SCL-90',
-      times: 773,
-    },
-    {
-      date: '2022-2',
-      scale: 'SCL-90',
-      times: 73,
-    },
-    {
-      date: '2022-3',
-      scale: 'SCL-90',
-      times: 273,
-    },
-    {
-      date: '2022-4',
-      scale: 'SCL-90',
-      times: 1073,
-    },
-    {
-      date: '2022-5',
-      scale: 'SCL-90',
-      times: 773,
-    },
-  ];
+const YearColumn: FC<YearColumnProps> = ({ resultByMonth }) => {
+  // const data = [
+  //   {
+  //     date: '2021-8',
+  //     scale: 'SCL-90',
+  //     times: 203,
+  //   },
+  //   {
+  //     date: '2021-9',
+  //     scale: 'SCL-90',
+  //     times: 803,
+  //   },
+  //   {
+  //     date: '2021-10',
+  //     scale: 'SCL-90',
+  //     times: 453,
+  //   },
+  //   {
+  //     date: '2021-11',
+  //     scale: 'SCL-90',
+  //     times: 343,
+  //   },
+  //   {
+  //     date: '2021-12',
+  //     scale: 'SCL-90',
+  //     times: 223,
+  //   },
+  //   {
+  //     date: '2022-1',
+  //     scale: 'SCL-90',
+  //     times: 773,
+  //   },
+  //   {
+  //     date: '2022-2',
+  //     scale: 'SCL-90',
+  //     times: 73,
+  //   },
+  //   {
+  //     date: '2022-3',
+  //     scale: 'SCL-90',
+  //     times: 273,
+  //   },
+  //   {
+  //     date: '2022-4',
+  //     scale: 'SCL-90',
+  //     times: 1073,
+  //   },
+  //   {
+  //     date: '2022-5',
+  //     scale: 'SCL-90',
+  //     times: 773,
+  //   },
+  // ];
   const config = {
-    data,
+    data: resultByMonth.data,
     // autoFit: true,
     xField: 'date',
-    yField: 'times',
-    seriesField: 'scale',
+    yField: 'cc',
+    seriesField: 'title',
     isGroup: true,
     // height: '150px',
     // width: '200',
@@ -100,4 +103,14 @@ const YearColumn: FC<YearColumnProps> = ({}) => {
   );
 };
 
-export default YearColumn;
+const mapStateToProps = ({
+  resultByMonth,
+}: {
+  resultByMonth: resultByMonthState;
+}) => {
+  return {
+    resultByMonth,
+  };
+};
+
+export default connect(mapStateToProps)(YearColumn);
--- src/pages/DV/services.ts
+++ src/pages/DV/services.ts
@@ -3,3 +3,7 @@ import request from '@/utils/request';
 export async function statics() {
   return request.get(`/api/dv/statics`, {});
 }
+
+export async function countResultByMonth() {
+  return request.get(`/api/dv/countResultByMonth`, {});
+}
